a4fa524fda6a4b3fdbd6cd8e339f91612c22c774
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2021-10-04 12:57:43 -0400

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,4 +1,4 @@
-/*const raining = true;
+const raining = true;
 const cold = false;
 
 if (raining) {
@@ -9,7 +9,7 @@ if (cold) {
   console.log("Make sure you pick out a scarf!");
 } else {
   console.log("Short sleeves are fine.");
-}*/
+}
 
 const temperature = -1;
 
@@ -22,3 +22,18 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea...");
+}
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
